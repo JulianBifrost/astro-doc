9f90f7098a61d7e3c84d0ba1a598dac3712d783e
JulianBifrost <13310123@itlp.edu.mx>
2026-08-30 20:29:39 -0700

    update route

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://julianbifrost.github.io',
-	base: 'astro-doc',
+	base: '/astro-doc',
 	integrations: [
 		starlight({
 			title: 'Docs/FED',
